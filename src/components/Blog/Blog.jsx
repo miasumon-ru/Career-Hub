073a9298d0141a0264c09150291d4428b34d5026
miasumon <miasumon.ru@gmail.com>
2024-03-25 22:47:00 +0600

    again deployment

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -5,7 +5,7 @@ const Blog = () => {
     return (
         <div>
 
-            <h1 className="text-5xl"> This is all about Blogs</h1>
+            <h1 className="text-5xl"> This is all  Blogs</h1>
             
         </div>
     );
